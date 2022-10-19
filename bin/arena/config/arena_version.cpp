1cfebafc1199947bf629ecefc3e66e02d7621ba7
Bhupendra Bhudia <bhupendra.bhudia@gmail.com>
2022-10-19 16:56:05 +0100

    Initial checkpoint - adjust logger_init

--- bin/arena/config/arena_version.cpp
+++ bin/arena/config/arena_version.cpp
@@ -3,7 +3,9 @@
 #include "arena/common/log.hpp"
 #include "arena/config/version.hpp"
 
-int main() {
+int main(int argc, char const **argv) {
+    arena::logger_init(argv[0]);
+
     spdlog::info("Project Version: {}", arena::version());
     spdlog::info("Project Version hash: {}", std::hash<std::string>{}(arena::version()));
 
--- include/arena/common/log.hpp
+++ include/arena/common/log.hpp
@@ -1,6 +1,8 @@
 #ifndef _ARENA_COMMON_LOG_HPP_
 #define _ARENA_COMMON_LOG_HPP_
 
+#include <filesystem>
+
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_sinks.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
@@ -16,13 +18,22 @@
 
 namespace arena {
 
-inline void logger_init() {
+inline void logger_init(char const *prog_name = nullptr) {
 #ifndef NDEBUG
     spdlog::set_level(spdlog::level::debug);
 #endif
+
+    // Log start-up
+    if (prog_name != nullptr) {
+        auto prog = std::filesystem::path(prog_name);
+#ifndef NDEBUG
+        spdlog::info("Starting {} [from {}]", prog.filename().c_str(), prog.parent_path().c_str());
+#else
+        spdlog::info("Starting {}", prog.filename().c_str());
+#endif
+    }
 }
 
 }  // namespace arena
-
 
 #endif  // _ARENA_COMMON_LOG_HPP_
